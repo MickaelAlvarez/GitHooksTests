ac97b67a7521d41fdeeeb15ed84336516c8dbcda
FlorianDoublet <florian.doublet@hotmail.fr>
2016-10-02 08:25:52 +0200

    test

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-import queue
-import threading
+import pexpect
 import subprocess
 
-proc = subprocess.Popen(['/usr/bin/git', 'pull'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+"""
+proc = subprocess.Popen(['/usr/bin/git', 'push'], stdout=subprocess.PIPE)
 
+stdout_value = proc.communicate()[0].decode("utf-8")
+if stdout_value == "" :
+	print("ya rien dedans :( ")
+else :
+	print("ya un truc " + stdout_value)
+"""
 
-r = proc.stdout.readline()
-print(r)
+child = pexpect.spawn ("git push")
+child.expect("Username for .*")
+print(child.before.decode("utf-8"))
+child.send("FlorianDoublet")
+print(child.read())
+child.expect("Password")
+print(child.before)
+child.send("Tu le sauras pas")
+print(" last " + child.after.decode("utf-8"))
